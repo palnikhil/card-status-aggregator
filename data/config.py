7fe5647a31d2a9035592b62629a3d548271ab485
palnikhil <nikhilpal9111@gmail.com>
2024-03-19 03:05:39 +0530

    Resolved path issue in etl_pipeline config

--- data/config.py
+++ data/config.py
@@ -1,10 +1,8 @@
-import os
-
 CONFIG = {
-    'DB_USER': os.environ.get('DB_USER'),
-    'DB_PASSWORD': os.environ.get('DB_PASSWORD'),
-    'DB_HOST': os.environ.get('DB_HOST'),
-    'DB_NAME': os.environ.get('DB_NAME'),
-    'DB_PORT': os.environ.get('DB_PORT'),
-    'DATA_PATH': '/data-service/csv_files/*.csv'
+    'DB_USER': "admin_user",
+    'DB_PASSWORD': "djjf_$^_rgo",
+    'DB_HOST': "localhost",
+    'DB_NAME': "zywadb",
+    'DB_PORT': "5432",
+    'DATA_PATH': 'csv_files/*.csv'
 }
